b807c453ca4ada40753dae7ae323c759429d798d
Patrick Eriksson <paleite@users.noreply.github.com>
2021-07-19 18:33:14 +0200

    Use the official polyfill

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,34 @@
+import { Temporal } from "@js-temporal/polyfill";
 import { temporalFromNow } from "./index";
 
+const now = Temporal.now;
+const temporalSpy = jest
+  .spyOn(now, "instant")
+  .mockImplementation(() => Temporal.Instant.from("2021-07-19T18:24:25Z"));
+const testInstant = Temporal.Instant.from("2021-07-19T12:58:12Z");
+
 describe("temporalFromNow", () => {
-  it("should launch without crashing", () => {
-    expect(temporalFromNow).not.toThrow();
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should run without crashing", () => {
+    expect(() => temporalFromNow(testInstant)).not.toThrow();
+  });
+
+  it("should return the correct string from the past", () => {
+    expect(temporalFromNow(testInstant)).toMatchInlineSnapshot(
+      `"19573 seconds ago"`
+    );
+
+    expect(temporalSpy).toHaveBeenCalledTimes(1);
+    expect(temporalSpy.mock.results[0]?.value).toBeInstanceOf(Temporal.Instant);
+  });
+
+  it("should return the correct string from the future", () => {
+    const futureInstant = Temporal.Instant.from("2021-08-19T18:24:25Z");
+    expect(temporalFromNow(futureInstant)).toMatchInlineSnapshot(
+      `"in 2678400 seconds"`
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,42 @@
-const temporalFromNow = async (): Promise<void> => {
-  let result;
-  // get parameters
-  console.log("TODO: getParameters");
+import { Temporal } from "@js-temporal/polyfill";
 
-  // calculate difference
-  console.log("TODO: calculateDifference");
+type PastFuture = "past" | "future";
 
-  // format result
-  console.log("TODO: formatResult");
+function assertNever(arg0: never): never {
+  throw new Error(`Invalid argument: ${JSON.stringify(arg0)}`);
+}
 
-  // return result
+const temporalFormat = (pf: PastFuture) => {
+  switch (pf) {
+    case "past":
+      return formatPast;
+    case "future":
+      return formatFuture;
+    default:
+      return assertNever(pf);
+  }
+};
+
+const formatFuture = (time: string) => `in ${time}`;
+const formatPast = (time: string) => `${time} ago`;
+const formatSeconds = (time: number) => `${Math.abs(time)} seconds`;
+
+const temporalFromNow = (instant: Temporal.Instant): string => {
+  const duration = instant.since(Temporal.now.instant(), {
+    largestUnit: "hour",
+    smallestUnit: "second",
+  });
+  const pastFuture: PastFuture = duration.sign === -1 ? "past" : "future";
+  const format = temporalFormat(pastFuture);
+
+  const result = format(
+    formatSeconds(
+      duration.round({
+        largestUnit: "second",
+        smallestUnit: "second",
+      })["seconds"]
+    )
+  );
   return result;
 };
 
